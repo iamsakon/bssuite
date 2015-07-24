f111d8164def52d841a499428a3663726a36fa06
sakon <sakon.bluechip@gmail.com>
2015-07-24 14:13:40 +0700

    add p:message and backing bean

--- bssuite/pms-parent/pms-web/src/main/java/com/swn/bss/pms/controller/AbstractMasterController.java
+++ bssuite/pms-parent/pms-web/src/main/java/com/swn/bss/pms/controller/AbstractMasterController.java
@@ -1,5 +1,6 @@
 package com.swn.bss.pms.controller;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.model.LazyDataModel;
@@ -39,4 +40,20 @@ public class AbstractMasterController<T> extends LazyDataModel<T>{
 	public void setBackScreen(String backScreen) {
 		this.backScreen = backScreen;
 	}
+	
+	public void info(String message) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", message));
+    }
+     
+    public void warn(String message) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning!", message));
+    }
+     
+    public void error(String message) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", message));
+    }
+     
+    public void fatal(String message) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", message));
+    }
 }
