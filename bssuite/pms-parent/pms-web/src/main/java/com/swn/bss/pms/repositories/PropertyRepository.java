d0ee655efdb113a59dffb36f66d5ad047f1b98a0
MrMai <sakon.bluechip@gmail.com>
2015-07-24 09:13:03 +0700

    -

--- bssuite/pms-parent/pms-web/src/main/java/com/swn/bss/pms/repositories/PropertyRepository.java
+++ bssuite/pms-parent/pms-web/src/main/java/com/swn/bss/pms/repositories/PropertyRepository.java
@@ -3,6 +3,8 @@
  */
 package com.swn.bss.pms.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -15,5 +17,6 @@ import com.swn.bss.pms.entity.PropertyDomain;
 public interface PropertyRepository extends
 		JpaRepository<PropertyDomain, Long>,
 		JpaSpecificationExecutor<PropertyDomain> {
-
+	
+	public List<PropertyDomain> findByIsActive(boolean isActive);
 }
